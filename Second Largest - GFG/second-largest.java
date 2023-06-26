dae982c24cd62b8a8a9e760c2ead6154daf0b854
eswarmamidi19 <109218033+eswarmamidi19@users.noreply.github.com>
2023-06-26 20:27:42 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.java
+++ Second Largest - GFG/second-largest.java
@@ -31,20 +31,59 @@ public class Main {
 
 
 //User function Template for Java
+// 0 1 2 3 4 5 6
+
+// 6 48 4 2 4 0 78   ans = 48
+//               ^             
+
+// fm = 78
+// sm = 48  sm!=fm ce>sm
+
+
+
+
+
+
+
 
 class Solution {
     int print2largest(int arr[], int n) {
         // code here
-        int max = Integer.MIN_VALUE;
+        
+        int firstMax = Integer.MIN_VALUE;
+        
         for(int i=0;i<n;i++){
-            max = Math.max(max,arr[i]);
+            if(arr[i] > firstMax){
+                firstMax =arr[i];
+            }
         }
-        int sm = Integer.MIN_VALUE;
+        
+        int secondMax = Integer.MIN_VALUE;
+        
         for(int i=0;i<n;i++){
-             if(sm<arr[i] && arr[i] < max){
-                 sm =arr[i];
-             }
+            if(arr[i] > secondMax && arr[i]!=firstMax){
+                 secondMax = arr[i];
+            }
         }
-        return sm==Integer.MIN_VALUE?-1 : sm;
+        
+        
+        if(secondMax==Integer.MIN_VALUE){
+            return -1;
+        }
+        
+        return secondMax;
+     
+     
+        // int max = Integer.MIN_VALUE;
+        // for(int i=0;i<n;i++){
+        //     max = Math.max(max,arr[i]);
+        // }
+        // int sm = Integer.MIN_VALUE;
+        // for(int i=0;i<n;i++){
+        //      if(sm<arr[i] && arr[i] < max){
+        //          sm =arr[i];
+        //      }
+        // }
+        // return sm==Integer.MIN_VALUE?-1 : sm;
     }
 }

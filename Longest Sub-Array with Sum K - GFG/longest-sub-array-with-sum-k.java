04a3b6a36b18b07d65f44c8a433cc6c4e4b0412d
eswarmamidi19 <109218033+eswarmamidi19@users.noreply.github.com>
2023-07-12 23:16:21 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int arr[], int n, int k) {
+        //Complete the function
+        
+        int sum = 0;
+        
+        int ans = 0;
+        
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        for(int i=0;i<n;i++){
+            
+            sum+=arr[i];
+            
+            if(sum==k){
+                ans = (int)Math.max(ans,i+1);
+            }
+            
+            if(hm.containsKey(sum-k)){
+                ans = (int)Math.max(i-hm.get(sum-k),ans);
+            }
+            if(hm.get(sum)==null){
+                hm.put(sum,i);
+            }
+        }
+        return ans;
+        
+        
+    }
+    
+    
+}
+
+
